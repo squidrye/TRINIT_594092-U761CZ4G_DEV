63a01388d01ba99ec2224f6eec9a88cdf67c6075
squidrye <mihirs167@gmail.com>
2023-02-11 10:47:31 +0530

    dashboard cards

--- lib/presentation/DashBoard.dart
+++ lib/presentation/DashBoard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:we_care/presentation/widget/CommonWidgets.dart';
 
 class DashBoard extends StatelessWidget {
   static const route = "/DashBoard";
@@ -14,14 +15,12 @@ class DashBoard extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
             color: Theme.of(context).primaryColorLight,
-            child:Row(
-              children: [
-
-              ],
+            child: Row(
+              children: [buildStatsCards()],
             ),
           ),
           Container(
-            margin:EdgeInsets.only(top:150),
+            margin: EdgeInsets.only(top: 200),
             height: double.infinity,
             width: double.infinity,
             child: Card(
@@ -30,6 +29,23 @@ class DashBoard extends StatelessWidget {
           )
         ],
       )),
+    );
+  }
+
+  Widget buildStatsCards() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Container(
+        height: 200,
+        child: Card(
+          child: Column(
+            children: [
+              Container(width: 200, child: XTitle(value: "03")),
+              Container(width: 200, child: XSubTitle(value: "Active Campaign"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
